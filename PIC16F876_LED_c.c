58e6bda15ec3c92042ca5075ca5dc78f27b7624d
Bacelis <eric.bacelis@gmail.com>
2021-07-01 03:50:01 -0500

    Create PIC16F876_LED_c.c

---
+++ PIC16F876_LED_c.c
@@ -0,0 +1,16 @@
+#include <16F876.h>
+#fuses XT,NOWDT
+#use delay( clock = 4000000)
+#use standard_io(B)
+void main()
+{
+    port_b_pullups (TRUE);
+    output_low(PIN_B1);
+    while (1)
+    {
+        if (input(PIN_B0) == 1)
+            output_low(PIN_B1);
+        else
+            output_high(PIN_Bl);
+    }
+}
